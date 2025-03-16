815860e485fb77a06cba66ae41f3cbafc3f71287
sayan4545 <sayanchatterjee3010@gmail.com>
2025-03-17 02:02:06 +0530

    Basic set up

---
+++ ThirtyOneApps/src/main/java/com/thirtyOneApps/ThirtyOneAppsApplication.java
@@ -0,0 +1,13 @@
+package com.thirtyOneApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirtyOneAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirtyOneAppsApplication.class, args);
+	}
+
+}
---
+++ ThirtyOneApps/src/test/java/com/thirtyOneApps/ThirtyOneAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirtyOneApps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirtyOneAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
